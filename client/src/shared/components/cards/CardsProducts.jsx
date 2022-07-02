c98809e6ae1e4ef392448b3a33fc70443726536d
lucas3k <lucamargostivan20@gmail.com>
2022-07-02 16:47:16 -0300

    add funcion modify estruture

--- client/src/shared/components/cards/CardsProducts.jsx
+++ client/src/shared/components/cards/CardsProducts.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Button,
   Card,
@@ -6,45 +8,56 @@ import {
   Typography,
 } from "@mui/material";
 
+import { FormDialog } from "../dialogs/Dialog";
+
 const CardsProducts = ({ products }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Card
-      sx={{
-        backgroundColor: "#DEDEDE",
-        m: 1,
-        flex: "1 0 300px",
-        boxShadow: "0 1px 3px #AAA",
-      }}
-    >
-      <CardContent>
-        <Typography variant="h4" component="h5" sx={{ color: "#131313" }}>
-          {products.nome}
-        </Typography>
-        <Typography
-          sx={{ fontSize: 14, color: "#333", fontWeight: "500" }}
-          paragraph
-        >
-          {products.descricao}
-        </Typography>
-        <Typography
-          paragraph
-          sx={{ fontSize: 20, color: "#333", fontWeight: "500" }}
-        >
-          {products.preco.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          })}
-        </Typography>
-        <CardActions>
-          <Button variant="contained" size="small">
-            Deletar
-          </Button>
-          <Button variant="contained" size="small">
-            Modificar
-          </Button>
-        </CardActions>
-      </CardContent>
-    </Card>
+    <>
+      <FormDialog open={open} setOpen={setOpen} />
+      <Card
+        sx={{
+          backgroundColor: "#DEDEDE",
+          m: 1,
+          flex: "1 0 300px",
+          boxShadow: "0 1px 3px #AAA",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h4" component="h5" sx={{ color: "#131313" }}>
+            {products.nome}
+          </Typography>
+          <Typography
+            sx={{ fontSize: 14, color: "#333", fontWeight: "500" }}
+            paragraph
+          >
+            {products.descricao}
+          </Typography>
+          <Typography
+            paragraph
+            sx={{ fontSize: 20, color: "#333", fontWeight: "500" }}
+          >
+            {products.preco.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </Typography>
+          <CardActions>
+            <Button variant="contained" size="small">
+              Deletar
+            </Button>
+            <Button variant="contained" size="small" onClick={handleOpen}>
+              Modificar
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
